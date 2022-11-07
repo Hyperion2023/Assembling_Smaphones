5efc900bde55cc9077b956cedb15a6027e7b983f
filippo <filibiondi2000@gmail.com>
2022-11-07 18:27:21 +0100

    Moved robotic arm move to the environment. Finished district division

--- Core/Utils/District.py
+++ Core/Utils/District.py
@@ -5,9 +5,13 @@ class District:
         self.height = height
         self.task = []
         self.mounting_points = []
+        self.robotic_arm = []
 
     def add_task(self, task):
         self.task.append(task)
 
     def add_mounting_point(self, mounting_point):
         self.mounting_points.append(mounting_point)
+
+    def add_robotic_arm(self, robotic_arm):
+        self.robotic_arm.append(robotic_arm)
--- Core/Utils/RoboticArm.py
+++ Core/Utils/RoboticArm.py
@@ -1,11 +1,9 @@
 class RoboticArm:
-    def __init__(self, mounting_point):
-        self.mounting_point = mounting_point
-        mounting_point.occupied = True
+    def __init__(self):
+        self.mounting_point = None
         self.path = []
         self.moves = []
 
-    # TODO move this method in the environment class
     @staticmethod
     def check_action(new_point, environment):
         if new_point[0] > environment.widht or new_point[0] < 0:
@@ -21,17 +19,7 @@ class RoboticArm:
                 if p == new_point:
                     return False
 
-    def move(self, action, environment):
-        if action not in ["U", "R", "D", "L", "W"]:
-            raise ValueError("action must be one of U R D L W")
-        self.moves.append(action)
-
-        last_point = self.path[-1]
-        if action == "U":
-            self.path.append((last_point[0], last_point[1] + 1))
-        elif action == "R":
-            self.path.append((last_point[0] + 1, last_point[1]))
-        elif action == "D":
-            self.path.append((last_point[0], last_point[1] - 1))
-        elif action == "L":
-            self.path.append((last_point[0] - 1, last_point[1]))
+    def mount(self, mounting_point):
+        self.mounting_point = mounting_point
+        mounting_point.occupied = True
+        self.path.append((mounting_point.x, mounting_point.y))
